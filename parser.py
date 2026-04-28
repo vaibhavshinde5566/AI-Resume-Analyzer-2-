f5e3580f271e317aa0714b51721cb8ee5db3d0fb
vaibhav shinde <155558693+vaibhavshinde5566@users.noreply.github.com>
2026-04-28 09:33:43 +0530

    Delete parser.py

--- parser.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-def extract_text_from_pdf(file_path):
-    text = ""
-    with pdfplumber.open(file_path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text() or ""
-    return text
-
